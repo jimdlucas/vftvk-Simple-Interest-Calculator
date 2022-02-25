5d14a8d2173713a9c06ecef18522ef2628172499
Lucas <jamesdl@nala.roche.com>
2022-02-25 13:26:36 -0800

    final version

--- script.js
+++ script.js
@@ -1,29 +1,32 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute() {
+    //Get the values and calculate 
+    var principal = parseFloat(document.getElementById("principal").value);
+    var rate = parseFloat(document.getElementById("rate").value);
+    var years = parseInt(document.getElementById("years").value);
+    var interest = principal * years * rate / 100;
+    var yearInTheFuture = new Date().getFullYear() + years;
+    //Create the Interest text
+    document.getElementById("result").innerHTML = "Interest : If you deposit <mark>" + principal + "</mark>,<br/>" +
+        "at an interest rate of <mark>" + rate + "</mark>,<br/>" +
+        "You will receive an amount of <mark>" + interest + "</mark>,<br/> " +
+        "in the year <mark>" + yearInTheFuture + "</mark>";
+
 }
-// Create a variable called 'principal' and assign to it the value of the input element "principal"
-var principal = document.getElementById("principal").value;
 
-// Create a variable called rate and assign to it the value of the input element "rate
-var rate = document.getElementById("rate").value;
-
-// Create a variable called years and assign to it the value of the input element "years"
-var years = document.getElementById("years").value;
-
-// Create a variable called interest and assign to it the value of principal * years * rate / 100
-var interest = principal * years * rate / 100;
-
-//Write the logic to convert the 'No of Years' into the actual year in the future.
-var year = new Date().getFullYear()+parseInt(years);
-
-//Write a function that reads the value of the range slider and displays it the <span>adjacent to the slider
+//update ther rate value
 function updateRate()
 {
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText=rateval;
 }
 
-document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
-        
+//Check for positive values
+function validateAmount() {
+    var principal = document.getElementById("principal").value;
+    var biggerThanZero = parseInt(principal) > 0;
+    if (!biggerThanZero) {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+    }
+
+}
